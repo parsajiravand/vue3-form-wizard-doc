00087c0a1b3ff27f772cf6ee090c2fd5f61a3a80
parsa jiravand <parsajiravand@gmail.com>
2022-09-05 13:14:25 +0430

    setup vue3-form-wizard  document in new repo

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,62 @@
+/* const { path } = require("@vuepress/utils"); */
+import { registerComponentsPlugin } from '@vuepress/plugin-register-components'
+import {  path } from '@vuepress/utils'
+import { defaultTheme } from 'vuepress'
+
+export default  {
+   theme: defaultTheme({
+  locales: {
+    "/": {
+      lang: "en-US",
+      title: "VuePress",
+      description: "Vue-powered Static Site Generator",
+    },
+  },
+    navbar: [
+      // NavbarItem
+      {
+        text: 'Foo',
+        link: '/foo/',
+      },
+      // NavbarGroup
+      {
+        text: 'Group',
+        children: ['/group/foo.md', '/group/bar.md'],
+      },
+      // string - page file path
+      '/bar/README.md',
+    ],
+    sidebar:[
+       // NavbarItem
+      {
+        text: 'Usage',
+        link: '/usage/',
+      },
+      {
+        text: 'Props',
+        link: '/props/',
+      },{
+        text: 'Slots',
+        link: '/slots/',
+      },{
+        text: 'Methods',
+        link: '/methods/',
+      },{
+        text: 'Scoped slots',
+        link: '/scoped-slots/',
+      },{
+        text: 'Demos',
+        link: '/demos/',
+      },{
+        text: 'Playground',
+        link: '/playground/',
+      },
+    ],
+
+}),
+ plugins: [
+    registerComponentsPlugin({
+      componentsDir: path.resolve(__dirname, './components'),
+    }),
+  ],
+};
