558a5b91aed54913d656c206774c7c35815214e5
parsa jiravand <parsajiravand@gmail.com>
2022-09-09 04:39:06 +0430

    add call function before tab switch sample

---
+++ docs/.vuepress/components/playground/call-function-before-tab-switch.vue
@@ -0,0 +1,33 @@
+<template>
+  <form-wizard @on-complete="onComplete" shape="tab" color="#9b59b6">
+    <tab-content
+      title="Personal details"
+      icon="ti-user"
+      :before-change="beforeTabSwitch"
+    >
+      My first tab content
+    </tab-content>
+    <tab-content title="Additional Info" icon="ti-settings">
+      My second tab content
+    </tab-content>
+    <tab-content title="Last step" icon="ti-check">
+      Yuhuuu! This seems pretty damn simple
+    </tab-content>
+  </form-wizard>
+</template>
+
+<script>
+export default {
+  name: "CallFunctionBeforeTabSwitch",
+
+  methods: {
+    onComplete: function () {
+      alert("Yay. Done!");
+    },
+    beforeTabSwitch: function () {
+      alert("This is called before switchind tabs");
+      return true;
+    },
+  },
+};
+</script>
