3dacc6745858347b07dd8432f7a3d832efd0fd96
parsajiravand <parsajiravand@gmail.com>
2023-07-17 12:37:29 +0330

    setup blog module

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,6 +4,7 @@ import { path } from "@vuepress/utils";
 import { defaultTheme } from "vuepress";
 import { googleAnalyticsPlugin } from "@vuepress/plugin-google-analytics";
 import * as dotenv from 'dotenv'
+import { blogPlugin } from "vuepress-plugin-blog2";
 
 // call dotenv and it will return an Object with a parsed key
 dotenv.config()
@@ -24,6 +25,27 @@ export default {
       {
         text: "Docs",
         link: "/usage/",
+      },
+      {
+        text:"Blog",
+        children:[
+          {
+            text: "Article",
+            link: "/article/",
+          },
+          {
+            text: "Category",
+            link: "/category/",
+          },
+          {
+            text: "Tag",
+            link: "/tag/",
+          },
+          {
+            text: "Timeline",
+            link: "/timeline/",
+          },
+        ]
       },
       
       // NavbarGroup
@@ -89,6 +111,99 @@ export default {
     googleAnalyticsPlugin({
       // options
       id: process.env.GOOGLE_API_TOKEN,
+    }),
+    blogPlugin({
+      // only files under posts are articles
+      filter: ({ filePathRelative }) =>
+        filePathRelative ? filePathRelative.startsWith("posts/") : false,
+
+      // getting article info
+      getInfo: ({ frontmatter, title, data }) => ({
+        title,
+        author: frontmatter["author"] || "",
+        date: frontmatter.date || null,
+        category: frontmatter["category"] || [],
+        tag: frontmatter["tag"] || [],
+        excerpt:
+          // support manually set excerpt through frontmatter
+          typeof frontmatter["excerpt"] === "string"
+            ? frontmatter["excerpt"]
+            : data?.["excerpt"] || "",
+      }),
+
+      // generate excerpt for all pages excerpt those users choose to disable
+      excerptFilter: ({ frontmatter }) =>
+        !frontmatter["home"] &&
+        frontmatter["excerpt"] !== false &&
+        typeof frontmatter["excerpt"] !== "string",
+
+      category: [
+        {
+          key: "category",
+          getter: (page) => page.frontmatter["category"] || [],
+          layout: "Category",
+          itemLayout: "Category",
+          frontmatter: () => ({ title: "Categories", sidebar: false }),
+          itemFrontmatter: (name) => ({
+            title: `Category ${name}`,
+            sidebar: false,
+          }),
+        },
+        {
+          key: "tag",
+          getter: (page) => page.frontmatter["tag"] || [],
+          layout: "Tag",
+          itemLayout: "Tag",
+          frontmatter: () => ({ title: "Tags", sidebar: false }),
+          itemFrontmatter: (name) => ({
+            title: `Tag ${name}`,
+            sidebar: false,
+          }),
+        },
+      ],
+
+      type: [
+        {
+          key: "article",
+          // remove archive articles
+          filter: (page) => !page.frontmatter["archive"],
+          path: "/article/",
+          layout: "Article",
+          frontmatter: () => ({ title: "Articles", sidebar: false }),
+          // sort pages with time and sticky
+          sorter: (pageA, pageB) => {
+            if (pageA.frontmatter["sticky"] && pageB.frontmatter["sticky"])
+              return pageB.frontmatter["sticky"] - pageA.frontmatter["sticky"];
+
+            if (pageA.frontmatter["sticky"] && !pageB.frontmatter["sticky"])
+              return -1;
+
+            if (!pageA.frontmatter["sticky"] && pageB.frontmatter["sticky"])
+              return 1;
+
+            if (!pageB.frontmatter.date) return 1;
+            if (!pageA.frontmatter.date) return -1;
+
+            return (
+              new Date(pageB.frontmatter.date).getTime() -
+              new Date(pageA.frontmatter.date).getTime()
+            );
+          },
+        },
+        {
+          key: "timeline",
+          // only article with date should be added to timeline
+          filter: (page) => page.frontmatter.date instanceof Date,
+          // sort pages with time
+          sorter: (pageA, pageB) =>
+            new Date(pageB.frontmatter.date).getTime() -
+            new Date(pageA.frontmatter.date).getTime(),
+          path: "/timeline/",
+          layout: "Timeline",
+          frontmatter: () => ({ title: "Timeline", sidebar: false }),
+        },
+      ],
+      hotReload: true,
     }),
   ],
   head: [
